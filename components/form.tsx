4dd0bd64b53e256b8efa6c1ce438bd9a2181f347
jyotiprakashmfsi <mfsi.jyotip@outlook.com>
2025-01-28 18:57:33 +0530

    first commit

---
+++ components/form.tsx
@@ -0,0 +1,12 @@
+export default function FormComponent(){
+    return (
+        <div>
+            <form action="">
+                <input type="text" />
+                <input type="email" name="" id="" />
+                <input type="number" name="" id="" />
+                <input type="button" value="Submit" />
+            </form>
+        </div>
+    )
+}
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import FormComponent from '../components/form'
+
+function App() {
+
+  return (
+    <>
+     <h1>Form</h1>
+     <FormComponent/>
+    </>
+  )
+}
+
+export default App
